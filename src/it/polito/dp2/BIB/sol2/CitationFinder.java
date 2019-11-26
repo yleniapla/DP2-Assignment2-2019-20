ae17e74cd4f9f407c4c7e3db6bce1655cd7a0ebd
ylepla <coccinella1995>
2019-11-26 12:43:06 +0100

    added first pieces from last year assignment in citationFinder

--- src/it/polito/dp2/BIB/sol2/CitationFinder.java
+++ src/it/polito/dp2/BIB/sol2/CitationFinder.java
@@ -3,16 +3,31 @@ package it.polito.dp2.BIB.sol2;
 import java.util.Properties;
 import java.util.Set;
 
+import it.polito.dp2.BIB.BibReader;
+import it.polito.dp2.BIB.BibReaderFactory;
 import it.polito.dp2.BIB.BookReader;
 import it.polito.dp2.BIB.ItemReader;
 import it.polito.dp2.BIB.JournalReader;
 import it.polito.dp2.BIB.ass2.ServiceException;
 import it.polito.dp2.BIB.ass2.UnknownItemException;
 
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.*;
+
 public class CitationFinder implements it.polito.dp2.BIB.ass2.CitationFinder {
 	
 	private static String serviceBaseUri = "";
 	private static String serviceBasePort = "";
+	
+	// private Client client;				
+	private boolean	status;				//system state
+	private BibReader 			monitor;	//interface on the Biblio
+	private BibReaderFactory	readFactory;
+
+	private Map<String,URI>		placeToUri;	//map placeID-URI
+	private Map<URI,String>		UriToPlace;	//map URI-placeID
+	private Map<String,NodeBody>	idToNode;		//map placeID-NodeBody
+	private Set<URI>			relations;	//all relations in the system
 
 	public CitationFinder(Properties prop) {
 		serviceBaseUri = prop.getProperty("it.polito.dp2.BIB.ass2.URL");
